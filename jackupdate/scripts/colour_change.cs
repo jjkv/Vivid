645aaab34826dadff83086743637aa434274fde6
jack <jackkerns@Jacks-MBP-60.fios-router.home>
2016-11-15 02:35:51 -0500

    updated plat script to work with physics, implemented respawn

--- jackupdate/scripts/colour_change.cs
+++ jackupdate/scripts/colour_change.cs
@@ -3,19 +3,29 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class colour_change : MonoBehaviour {
-	public on_update script;
+	public Update_plats Update_plats;
+	public Update_intrs Update_intrs;
 	public Light lite;
-	public Transform interacts;
-	public 
+	public Transform Platforms;
+	public Transform Interacts;
+
+	public bool enabled;
+	public bool r_enabled;
+	public bool b_enabled;
+	public bool g_enabled;
+	public bool rclick_enabled;
+
 	//	public Text col_text;
 	// Use this for initialization
 	void Start () {
 		lite.color = Color.white;
-		update_objects ();
+		update_objects();
+		//	col_text.text = "Current Lens: White";
 	}
 
 	void change()
 	{
+
 		if (lite.color == Color.white) {
 			lite.color = Color.red;
 			//			col_text.text = "Current Lens: Red";
@@ -33,10 +43,12 @@ public class colour_change : MonoBehaviour {
 
 	void update_objects() 
 	{
-		foreach (Transform rend in interacts) {
+		foreach (Transform rend in Platforms) {
 			string cooler = rend.tag;
-			if (cooler == "white")
-				rend.GetComponent<Renderer>().material.color = lite.color;
+			if (cooler == "white") {
+				rend.GetComponent<Renderer> ().material.color = lite.color;
+				rend.GetComponent<Renderer> ().material.SetColor("_EmissionColor", lite.color);
+			}
 			if (cooler == "red")
 				update_r (rend);
 			if (cooler == "blue")
@@ -51,39 +63,83 @@ public class colour_change : MonoBehaviour {
 				update_c (rend);
 			if (cooler == "black")
 				update_black (rend);
-			script.on_change (rend);
+			Update_plats.on_change (rend);
+		}
+		foreach (Transform rend in Interacts) {
+			string cooler = rend.tag;
+			if (cooler == "white") {
+				rend.GetComponent<Renderer> ().material.color = lite.color;
+				rend.GetComponent<Renderer> ().material.SetColor("_EmissionColor", lite.color);
+			}
+			if (cooler == "red")
+				update_r (rend);
+			if (cooler == "blue")
+				update_b (rend);
+			if (cooler == "green")
+				update_g (rend);
+			if (cooler == "yellow")
+				update_y (rend);
+			if (cooler == "magenta")
+				update_m (rend);
+			if (cooler == "cyan")
+				update_c (rend);
+			if (cooler == "black")
+				update_black (rend);
+			Update_intrs.on_change (rend);
 		}
 	}
 
 	void restore_color(Transform rend)
 	{
-		if (rend.tag == "white")
+		if (rend.tag == "white") {
 			rend.GetComponent<Renderer> ().material.color = Color.white; 
-		if (rend.tag == "red")
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.white);
+		}
+		if (rend.tag == "red") {
 			rend.GetComponent<Renderer> ().material.color = Color.red; 
-		if (rend.tag == "green")
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.red);
+		}
+		if (rend.tag == "green") {
 			rend.GetComponent<Renderer> ().material.color = Color.green; 
-		if (rend.tag == "blue")
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.green);
+		}
+		if (rend.tag == "blue"){
 			rend.GetComponent<Renderer> ().material.color = Color.blue; 
-		if (rend.tag == "yellow")
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.blue);
+		}
+		if (rend.tag == "yellow") {
 			rend.GetComponent<Renderer> ().material.color = Color.yellow; 
-		if (rend.tag == "magenta")
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.yellow);
+		}
+		if (rend.tag == "magenta"){
 			rend.GetComponent<Renderer> ().material.color = Color.magenta; 
-		if (rend.tag == "cyan")
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.magenta);
+		}
+		if (rend.tag == "cyan"){
 			rend.GetComponent<Renderer> ().material.color = Color.cyan; 
-		if (rend.tag == "black")
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.cyan);
+		}
+		if (rend.tag == "black") {
 			rend.GetComponent<Renderer> ().material.color = Color.black; 
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.black);
+		}
 	}
 	void update_r(Transform rend)
 	{
 		if (lite.color == Color.white)
 			restore_color (rend);
-		if (lite.color == Color.red)
+		if (lite.color == Color.red) {
 			rend.GetComponent<Renderer> ().material.color = Color.white;
-		if (lite.color == Color.green)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.white);
+		}
+		if (lite.color == Color.green) {
 			rend.GetComponent<Renderer> ().material.color = Color.yellow;
-		if (lite.color == Color.blue)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.yellow);
+		}
+		if (lite.color == Color.blue) {
 			rend.GetComponent<Renderer> ().material.color = Color.magenta;
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.magenta);
+		}
 	}
 
 	void update_b(Transform rend)
@@ -91,13 +147,18 @@ public class colour_change : MonoBehaviour {
 		if (lite.color == Color.white){
 			restore_color (rend);
 		}
-		if (lite.color == Color.red)
+		if (lite.color == Color.red) {
 			rend.GetComponent<Renderer> ().material.color = Color.magenta;
-		if (lite.color == Color.green)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.magenta);
+		}
+		if (lite.color == Color.green) {
 			rend.GetComponent<Renderer> ().material.color = Color.cyan;
-		if (lite.color == Color.blue)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.green);
+		}
+		if (lite.color == Color.blue) {
 			rend.GetComponent<Renderer> ().material.color = Color.white;
-
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.white);
+		}
 
 	}
 
@@ -105,65 +166,110 @@ public class colour_change : MonoBehaviour {
 	{
 		if (lite.color == Color.white)
 			restore_color (rend);
-		if (lite.color == Color.red)
+		if (lite.color == Color.red) {
 			rend.GetComponent<Renderer> ().material.color = Color.yellow;
-		if (lite.color == Color.green)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.yellow);
+		}
+		if (lite.color == Color.green) {
 			rend.GetComponent<Renderer> ().material.color = Color.white;
-		if (lite.color == Color.blue)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.white);
+		}
+		if (lite.color == Color.blue) {
 			rend.GetComponent<Renderer> ().material.color = Color.cyan;
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.cyan);
+		}
 	}
 
 	void update_y(Transform rend)
 	{
 		if (lite.color == Color.white)
 			restore_color (rend);
-		if (lite.color == Color.red)
+		if (lite.color == Color.red) {
 			rend.GetComponent<Renderer> ().material.color = Color.green;
-		if (lite.color == Color.green)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.green);
+		}
+		if (lite.color == Color.green) {
 			rend.GetComponent<Renderer> ().material.color = Color.red;
-		if (lite.color == Color.blue)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.red);
+		}
+		if (lite.color == Color.blue) {
 			rend.GetComponent<Renderer> ().material.color = Color.black;
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.black);
+		}
 	}
 	void update_c(Transform rend)
 	{
 		if (lite.color == Color.white)
 			restore_color (rend);
-		if (lite.color == Color.red)
+		if (lite.color == Color.red) {
 			rend.GetComponent<Renderer> ().material.color = Color.black;
-		if (lite.color == Color.green)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.black);
+		}
+		if (lite.color == Color.green) {
 			rend.GetComponent<Renderer> ().material.color = Color.blue;
-		if (lite.color == Color.blue)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.blue);
+		}
+		if (lite.color == Color.blue) {
 			rend.GetComponent<Renderer> ().material.color = Color.green;
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.green);
+		}
 	}
 	//magenta
 	void update_m(Transform rend)
 	{
 		if (lite.color == Color.white)
 			restore_color (rend);
-		if (lite.color == Color.red)
+		if (lite.color == Color.red) {
 			rend.GetComponent<Renderer> ().material.color = Color.blue;
-		if (lite.color == Color.green)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.blue);
+		}
+		if (lite.color == Color.green) {
 			rend.GetComponent<Renderer> ().material.color = Color.black;
-		if (lite.color == Color.blue)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.black);
+		}
+		if (lite.color == Color.blue) {
 			rend.GetComponent<Renderer> ().material.color = Color.red;
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.red);
+		}
 	}
 	void update_black(Transform rend)
 	{
 		if (lite.color == Color.white)
 			restore_color (rend);
-		if (lite.color == Color.red)
+		if (lite.color == Color.red) {
 			rend.GetComponent<Renderer> ().material.color = Color.cyan;
-		if (lite.color == Color.green)
-			rend.GetComponent<Renderer> ().material.color = Color.yellow;
-		if (lite.color == Color.blue)
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.cyan);
+		}
+		if (lite.color == Color.green) {
 			rend.GetComponent<Renderer> ().material.color = Color.magenta;
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.magenta);
+		}
+		if (lite.color == Color.blue) {
+			rend.GetComponent<Renderer> ().material.color = Color.yellow;
+			rend.GetComponent<Renderer> ().material.SetColor ("_EmissionColor", Color.yellow);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Mouse0)) {
-			change ();
-			update_objects ();
+		if (enabled) {
+			if (Input.GetKeyDown (KeyCode.Mouse1) && rclick_enabled) {
+				change ();
+				update_objects ();
+			} else if (Input.GetKeyDown (KeyCode.C) && r_enabled) {
+				lite.color = Color.red;
+				update_objects ();
+			} else if (Input.GetKeyDown (KeyCode.V) && b_enabled) {
+				lite.color = Color.blue;
+				update_objects ();
+			} else if (Input.GetKeyDown (KeyCode.B) && g_enabled) {
+				lite.color = Color.green;
+				update_objects ();
+			} else if (Input.GetKeyDown (KeyCode.N)) {
+				lite.color = Color.white;
+				update_objects ();
+			}
 		}
 	}
+
 }
---
+++ jackupdate/scripts/level1_5.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class level1_5 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			SceneManager.LoadScene ("_scenes/1.5");
+		}
+	}
+}

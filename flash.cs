c5ddba227fe3c13279c75a3661fae9339023df51
jack <jackkerns@wr-130-64-193-210.medford.tufts.edu>
2016-12-06 18:20:16 -0500

    updated flash animation

--- flash.cs
+++ flash.cs
@@ -1,19 +1,18 @@
-// this doesn't look great, but it's a start
-// make a point light as the child of the lamp's point light
-// and assign it to the flashLight variable. put this script
-// on the character capsule object, then give the colour_change
-// script the character in the flash script field.
-
 using UnityEngine;
 using System.Collections;
 
 public class flash : MonoBehaviour {
+	private Vector3 sphereAccel = new Vector3(5.0f, 5.0f, 5.0f);
+	private int lightAccel = 10;
+	private int lightBoundry = 200;
 
 	private Color sentinel;
 	public bool flashing;
 	public Light flashLight;
+	public GameObject flashSphere;
 
 	void Start () {
+		flashSphere.GetComponent<Transform> ().localScale = new Vector3 (0.0f, 0.0f, 0.0f);
 		flashLight.range = 0;
 		flashLight.intensity = 8;
 		flashLight.bounceIntensity = 8;
@@ -26,15 +25,19 @@ public class flash : MonoBehaviour {
 		flashing = true;
 		flashLight.color = c;
 		flashLight.range = 0;
+		flashSphere.GetComponent<Renderer> ().material.SetColor ("_TintColor", c);
+		flashSphere.GetComponent<Transform> ().localScale = new Vector3 (0.0f, 0.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (flashing) {
-			flashLight.range += 10;
-			if (flashLight.range >= 100) {
+			flashLight.range += lightAccel;
+			flashSphere.GetComponent<Transform> ().localScale += sphereAccel;
+			if (flashLight.range >= lightBoundry){
 				flashing = false;
 				flashLight.range = 0;
+				flashSphere.GetComponent<Transform> ().localScale = new Vector3 (0.0f, 0.0f, 0.0f);
 			}
 		}
 	}
